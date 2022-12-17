49cd14c2ffd5e05058a1b41a2417b9525fbb8dce
czhou578 <colizu2020@gmail.com>
2022-12-16 16:29:51 -0800

    get rid of unneeded go checks

--- go-backend/api/main.go
+++ go-backend/api/main.go
@@ -23,31 +23,31 @@ func getAllShippers(c *gin.Context) {
 	shippers := models.GetShippers()
 	fmt.Print(shippers)
 
-	if shippers == nil || len(shippers) == 0 {
-		c.AbortWithStatus(http.StatusNotFound)
-	} else {
-		// fmt.Print(c.IndentedJSON(http.StatusOK, shippers))
-		c.Writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-		c.IndentedJSON(http.StatusOK, shippers)
-	}
+	c.Writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+	c.IndentedJSON(http.StatusOK, shippers)
+	// if shippers == nil || len(shippers) == 0 {
+	// 	c.AbortWithStatus(http.StatusNotFound)
+	// } else {
+	// 	// fmt.Print(c.IndentedJSON(http.StatusOK, shippers))
+	// }
 }
 
 func getEmployeeIDCount(c *gin.Context) {
 	countId := models.GetCountNumId()
 
-	if countId == nil || len(countId) == 0 {
-		c.AbortWithStatus(http.StatusNotFound)
-	} else {
-		c.IndentedJSON(http.StatusOK, countId)
-	}
+	c.IndentedJSON(http.StatusOK, countId)
+	// if countId == nil || len(countId) == 0 {
+	// 	c.AbortWithStatus(http.StatusNotFound)
+	// } else {
+	// }
 }
 
 func newCategory(c *gin.Context) {
 	lastId := models.NewCategoryInsert()
 
-	if lastId == 0 {
-		c.AbortWithStatus(http.StatusNotFound)
-	} else {
-		c.IndentedJSON(http.StatusOK, lastId)
-	}
+	c.IndentedJSON(http.StatusOK, lastId)
+	// if lastId == 0 {
+	// 	c.AbortWithStatus(http.StatusNotFound)
+	// } else {
+	// }
 }
